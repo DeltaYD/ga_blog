15e36b6d268fcdcbaea92ce567d021b9ee4ebd0e
DeltaYD <akira_hirakawa@usp.br>
2022-02-27 18:03:46 -0300

    aa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   purge: [`_site/**/*.html`],
+  safelist: [
+    'bg-indigo-500',
+    'flex',
+    'h-screen',
+    'items-center',
+    'justify-center',
+    'w-full',
+  ],
   theme: {
     extend: {},
   },
